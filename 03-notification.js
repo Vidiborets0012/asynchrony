9990d92c9d1ec8eb22fe9b456851e89bcc740e8b
Vidiborets0012 <vidiboretsv@gmail.com>
2025-09-30 17:20:53 +0000

    Deploying to gh-pages from @ Vidiborets0012/asynchrony@486a04eb68a9de7425ddab51a1ae091123f51531 🚀

---
+++ 03-notification.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */const e=3e3,i=document.querySelector(".js-alert");let t=null;i.addEventListener("click",c);n();function n(){i.classList.add("is-visible"),t=setTimeout(()=>{console.log("setTimeOut"),o()},e)}function c(){o(),clearTimeout(t)}function o(){i.classList.remove("is-visible")}
+//# sourceMappingURL=03-notification.js.map
