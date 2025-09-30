486a04eb68a9de7425ddab51a1ae091123f51531
Vidiborets Viktor <vidiboretsv@gmail.com>
2025-09-30 20:20:26 +0300

    додати до уроку 1 Модуля 10 приклад застосування setTimeout

---
+++ src/js/03-notification.js
@@ -0,0 +1,38 @@
+/**
+ *
+ * - Показуємо та ховаємо, додаючи/видаляючи клас is-visible
+ * - Ховаємо через певний час
+ * - Ховаємо при кліці
+ * - Не забуваємо чистити таймер
+ *
+ */
+
+const NOTIFICATION_DELAY = 3000;
+
+const notification = document.querySelector('.js-alert');
+let timerId = null;
+
+notification.addEventListener('click', onNotificationClick);
+
+showNotification();
+
+function showNotification() {
+  notification.classList.add('is-visible');
+
+  timerId = setTimeout(() => {
+    console.log('setTimeOut');
+
+    // notification.classList.remove('is-visible');
+    hideNotification();
+  }, NOTIFICATION_DELAY);
+}
+
+function onNotificationClick() {
+  // notification.classList.remove('is-visible');
+  hideNotification();
+  clearTimeout(timerId);
+}
+
+function hideNotification() {
+  notification.classList.remove('is-visible');
+}
