169c8e0cebbbc06a3a275676807d23c3be07d02e
Vidiborets Viktor <vidiboretsv@gmail.com>
2025-09-30 20:44:36 +0300

    додати до уроку 1 Модуля 10 тему: дата та час

---
+++ src/js/04-date.js
@@ -0,0 +1,34 @@
+/*
+- Створення
+- Unix-час
+- Методи
+- Різниця часу
+- Date.now
+*/
+
+// const currentTime = new Date();
+// console.log('currentTime:', currentTime);
+// console.log('currentTime.getDate:', currentTime.getDate());
+// console.log('currentTime.getDay:', currentTime.getDay());
+// console.log('currentTime.getMonth:', currentTime.getMonth());
+// console.log('currentTime.getFullYear:', currentTime.getFullYear());
+
+// currentTime.setFullYear(2007);
+// console.log('currentTime:', currentTime);
+
+// // ********************
+// console.log(currentTime.getTime());
+// console.log(Date.now());
+
+// const date1 = Date.now();
+
+// setTimeout(() => {
+//   const date2 = Date.now();
+
+//   console.log(date2 - date1);
+// }, 2000);
+
+// const date3 = new Date('2007-07-07');
+// const date4 = new Date('2024-07-07');
+
+// console.log(date4 - date3);
