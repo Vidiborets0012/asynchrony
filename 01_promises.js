25d19b36f97460732ca686886da66a7d3cf22170
Vidiborets0012 <vidiboretsv@gmail.com>
2025-10-01 18:10:48 +0000

    Deploying to gh-pages from @ Vidiborets0012/asynchrony@2e0f1f3329888645e9da52e5c95460ab385581d5 🚀

---
+++ 01_promises.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";console.log("*****************");console.log("*****************");console.log("*****************");console.log("*****************");
+//# sourceMappingURL=01_promises.js.map
